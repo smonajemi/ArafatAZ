6c26abf5412a44665a0709e1adf16c39f1376df6
smonajemi <sina.monajemi@me.com>
2021-03-28 15:23:34 -0400

    commit

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,23 +1,63 @@
-// var x = 1;
-// document.getElementById("remove").style.visibility="hidden";
-// function addText(){
-//     // document.getElementById("remove").style.background="red";
-//     document.getElementById("remove").style.visibility="visible";
-//     if (localStorage.clickcount) {
-//         localStorage.clickcount = Number(localStorage.clickcount)+1;
-//     }
-//     document.getElementById("addText").innerHTML += "<li><input type='text' id='textbox'"+ x++ +"'></li>";
-   
-// }
-
-// function removeText(){
-//     var elmnt = document.getElementById("addText");
-//     elmnt.removeChild(elmnt.lastElementChild);
-//     if(elmnt.firstChild == null){
-//         document.getElementById("remove").style.visibility="hidden";
-//     }
-// }
-
-
-
+window.history.forward();
+function noBack() { 
+   setTimeout(0)
+   window.history.forward();
+ }
+ 
+  document.onkeydown = function(e) {
+    if(e.keyCode == 123) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+       return false;
+    }
+  }
+  document.onkeypress = function(e) {
+   if(e.keyCode == 123) {
+      return false;
+   }
+   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+      return false;
+   }
+   if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+      return false;
+   }
+   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+      return false;
+   }
+   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+      return false;
+   }
+ }
+ document.onmousedown = function(e) {
+   if(e.keyCode == 123) {
+      return false;
+   }
+   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+      return false;
+   }
+   if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+      return false;
+   }
+   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+      return false;
+   }
+   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+      return false;
+   }
+ }  
   
+var div = document.createElement('div');
+Object.defineProperty(div,'id',{get:function(){
+      window.location.replace("https://www.google.com");
+}});
+setTimeout(()=>console.log(div),1000)
