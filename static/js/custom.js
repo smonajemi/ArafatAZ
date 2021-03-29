c0c563c26f961a011222c413bf914254d866af21
smonajemi <sina.monajemi@me.com>
2021-03-29 01:23:20 -0400

    commit

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,27 @@
+// ANIMATE
+
+var width = $(window).width();
+function goRight() {
+  $("#animate").animate({
+  left: width
+}, 100000, function() {
+  setTimeout(goLeft, 0);
+});
+}
+function goLeft() {
+  $("#animate").animate({
+  left: 0
+}, 100, function() {
+   setTimeout(goRight, 0);
+});
+}
+
+setTimeout(goRight, 0);
+
+$("#animate").on('click',function(){$('#animate').stop();
+                goLeft();
+})  
+
 window.history.forward();
 function noBack() { 
    setTimeout(0)
